a7fe6f3ed20c199c4522edfb5c2437dfd432bac9
jolt <jolt>
2005-04-06 17:47:46 +0000

    Provide helper to be able to compile the generated code

---
+++ tools/mipsdec/mipsdec_helper.h
@@ -0,0 +1,25 @@
+#ifndef MIPSEC_HELPER_H
+#define MIPSEC_HELPER_H
+
+static unsigned REG_a0;
+static unsigned REG_a1;
+static unsigned REG_a2;
+static unsigned REG_a3;
+
+static unsigned REG_sp;
+static unsigned REG_ra;
+static unsigned REG_at;
+static unsigned REG_gp;
+
+static unsigned REG_s0;
+static unsigned REG_s1;
+static unsigned REG_s2;
+static unsigned REG_s3;
+
+static unsigned REG_v0;
+static unsigned REG_v1;
+
+static unsigned read_c0_status(void) { return 0; }
+static void write_c0_status(unsigned uVal) {}
+
+#endif
