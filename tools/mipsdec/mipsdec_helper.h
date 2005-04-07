b5ffd5ae508fd641073df5168fe293fa7641559d
jolt <jolt>
2005-04-07 10:23:55 +0000

    Add some more registers

--- tools/mipsdec/mipsdec_helper.h
+++ tools/mipsdec/mipsdec_helper.h
@@ -10,11 +10,21 @@ static unsigned REG_sp;
 static unsigned REG_ra;
 static unsigned REG_at;
 static unsigned REG_gp;
+static unsigned REG_fp;
 
 static unsigned REG_s0;
 static unsigned REG_s1;
 static unsigned REG_s2;
 static unsigned REG_s3;
+static unsigned REG_s4;
+static unsigned REG_s5;
+static unsigned REG_s6;
+static unsigned REG_s7;
+
+static unsigned REG_t0;
+static unsigned REG_t1;
+static unsigned REG_t2;
+static unsigned REG_t3;
 
 static unsigned REG_v0;
 static unsigned REG_v1;
